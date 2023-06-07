be763d0cce4b107db09e1ecc6ef4a5ca9a6109a6
Mehul <mehul_angle@hotmail.com>
2023-06-06 21:41:25 -0400

    complete file

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+
+}
+function appendCat(name) {
+     const newArr = [...cats, name];
+     return newArr;
+
+}
+
+function prependCat(name) {
+    const newArr = [name, ...cats];
+    return newArr;
+
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+
